8d1bc7258597ee605f96f0fcd8931a3cb1e0126a
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-01-03 14:36:17 +0900

    [Silver IV] Title: 듣보잡, Time: 240 ms, Memory: 30356 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.js
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let [a,b] = input.shift().split(" ").map((el) => Number(el))
+let [a,b] = input.shift().split(" ").map(Number)
 
 let nHear = new Set(input.slice(0,a))
 let nSee = new Set(input.slice(a))
 const common = [...nHear].filter(x => nSee.has(x)).sort()
 
-console.log(common.length)
-for(let name of common){
-    console.log(name)
-}
+console.log(`${common.length}\n${common.join('\n')}`)
