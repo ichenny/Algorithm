2bd53dfe49589af68b1c643c697d43acaae70511
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-01-04 11:44:38 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,6 +1,3 @@
 function solution(str1, str2) {
-    let answer
-    let isInclude = str1.search(str2)
-    isInclude === -1 ? answer = 2 : answer = 1
-    return answer
+    return str1.search(str2) === -1 ? 2 : 1
 }
