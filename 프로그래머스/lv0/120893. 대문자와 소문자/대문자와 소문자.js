470b145b5794b8a45e8752d80191eccd551d89d2
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-01-05 18:44:33 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.27 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -1,9 +1,3 @@
 function solution(my_string) {
-    let arr = [] 
-    for(let i=0; i<my_string.length; i++){
-        let letter = my_string[i]
-        if(letter === letter.toUpperCase()) arr.push(letter.toLowerCase())
-        else if(letter === letter.toLowerCase()) arr.push(letter.toUpperCase())
-    }
-    return arr.join("")
+    return [...my_string].map((el) => el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase()).join("")
 }
