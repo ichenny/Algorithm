9b04a29f8fcb7b8030cfdcf84d0b36612aae85fb
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-01-09 11:29:42 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(my_string, num1, num2) {
+    let answer = ''
+    let str1 = my_string[num1]
+    let str2 = my_string[num2]
+    for(let i=0; i<my_string.length; i++){
+        if(i === num1) answer += str2
+        else if(i === num2) answer += str1
+        else answer += my_string[i]
+    }
+    return answer
+}
