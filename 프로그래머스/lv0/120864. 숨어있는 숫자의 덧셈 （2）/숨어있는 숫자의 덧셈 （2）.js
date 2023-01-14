bff0b92a81fcd4cec00d9ce1ec79bd920e629e60
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-01-14 20:02:20 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,8 +1,5 @@
 function solution(my_string) {
     let regex = /[^0-9]/g;				
-    let arr = (my_string.replace(regex, "!")).split("!").filter((el) => el.length > 0).map(Number)
-    if(arr.length === 1) return Number(arr[0])
-    else if(arr.length > 1) return arr.reduce((a,b) => a+b)
-    else if(!arr.length) return 0
+    return my_string.replace(regex, " ").split(" ").map(Number).reduce((a,b)=>a+b)
     
 }
