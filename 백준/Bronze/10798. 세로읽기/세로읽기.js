0197446f690ce0842d8e165bc10dec888d8a1088
Chaeeun Lee <102204957+ichenny@users.noreply.github.com>
2023-04-20 10:46:43 +0900

    [Bronze I] Title: 세로읽기, Time: 132 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.js
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -1,10 +1,6 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const maxLeng = Math.max(...input.map((el) => el.length))
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 let answer = ""
-
-for(let i=0; i<maxLeng; i++){
+for(let i=0; i<15; i++){
     const word = input.map((el) => el[i]).filter((el) => el !== undefined).join("")
     answer += word
 }
